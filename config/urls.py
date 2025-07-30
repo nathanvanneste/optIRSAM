64c371eb8d91b95e1215823da4744c1ff07a00e4
Nathan VANNESTE <navanneste@irsam.fr>
2025-07-30 09:38:48 +0200

    Ajout d'une classe d'interface appelée core => gérer ce qui ne l'est pas dans les autres classes

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('core.urls')),
     path('users/',include('user.urls')),
     path('solver/',include('solver.urls')),
     path('reports/',include('reports.urls')),
