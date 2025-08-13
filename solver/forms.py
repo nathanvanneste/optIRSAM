4d35136e1fd69399ba9b1db8e632eaf4ed55a810
Nathan VANNESTE <navanneste@irsam.fr>
2025-08-13 13:59:18 +0200

    MAJ boutons + corrections textuelles

--- solver/forms.py
+++ solver/forms.py
@@ -5,3 +5,4 @@ class RunForm(forms.ModelForm):
     class Meta:
         model = Run
         fields = ['groupe', 'etablissement', 'calculation_mod','mode','capacity', 'time_limit']
+        labels = {'groupe' : 'Groupe', 'etablissement' : 'Etablissement', 'calculation_mod' : 'Paramètre de coût', 'mode' : 'Depôt', 'capacity' : 'Capacité des véhicules', 'time_limit' : 'Temps de résolution'}
